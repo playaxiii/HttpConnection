23df98ab999b1f7ac28418366b5d9d30162088cb
Avinash Sharma <play.axiii@gmail.com>
2023-11-21 17:57:38 +0530

    Initial commit

---
+++ app/src/main/java/com/axiii/httpconnection/MainActivity.java
@@ -0,0 +1,129 @@
+package com.axiii.httpconnection;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
+    private List<MyDataModel> dataList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new MyAdapter(dataList);
+        recyclerView.setAdapter(adapter);
+
+        new GetDataTask().execute();
+    }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://jsonplaceholder.typicode.com/posts")
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                try {
+                    JSONArray jsonArray = new JSONArray(result);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String title = jsonObject.getString("title");
+
+                        MyDataModel data = new MyDataModel(title);
+                        dataList.add(data);
+                    }
+
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class MyDataModel {
+        private String title;
+
+        public MyDataModel(String title) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+
+    public static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        private List<MyDataModel> dataList;
+
+        public MyAdapter(List<MyDataModel> dataList) {
+            this.dataList = dataList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            MyDataModel data = dataList.get(position);
+            holder.titleTextView.setText(data.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataList.size();
+        }
+
+        public static class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView titleTextView;
+
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                titleTextView = itemView.findViewById(R.id.itemNameTextView);
+            }
+        }
+    }
+}
